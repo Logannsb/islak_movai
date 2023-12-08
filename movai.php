4eed4178e658d45475e073105050ef864ea9c32f
Logan Nussbaum <81440110+Logannsb@users.noreply.github.com>
2023-12-08 03:56:43 +0100

    Add files via upload

---
+++ movai.php
@@ -0,0 +1,444 @@
+<?php
+
+function a() {
+    echo "oscour\n";
+    b();
+}
+
+function b() {
+    echo "oscour\n";
+    c();
+}
+
+function c() {
+    echo "eh\n";
+    d();
+}
+
+function d() {
+    echo "Fonction ya\n";
+    e();
+}
+
+function e() {
+    echo "Fonction 9\n";
+    f();
+}
+
+function f() {
+    echo "35 Pleiu\n";
+    g();
+}
+
+function g() {
+    echo "Fonction 15\n";
+    h();
+}
+
+function h() {
+    echo "Fonction 8\n";
+    i();
+}
+
+function i() {
+    echo "Fonction 1\n";
+    j();
+}
+
+function j() {
+    echo "Fonction 53\n";
+    k();
+}
+
+function k() {
+    echo "Fonction  de 14\n";
+    l();
+}
+
+function l() {
+    echo "Fonction 7\n";
+    m();
+}
+
+function m() {
+    echo "Fonction fonction !55\n";
+    n();
+}
+
+function n() {
+    echo "Fonction 2\n";
+    o();
+}
+
+function o() {
+    echo "Fonction 19\n";
+    p();
+}
+
+function p() {
+    echo "Fonction 3\n";
+    q();
+}
+
+function q() {
+    echo "Fonction 34\n";
+    r();
+}
+
+function r() {
+    echo "Fonction 4\n";
+    a();
+}
+
+function s() {
+    echo "Fonction 58\n";
+}
+
+function t() {
+    echo "Fonction 23\n";
+    u();
+}
+
+function u() {
+    echo "Fonction 11 3 8 88thx\n";
+    v();
+}
+
+function v() {
+    echo "Fonction Alèd 52\n";
+    w();
+}
+
+function w() {
+    echo "Fonction 51\n";
+    x();
+}
+
+function x() {
+    echo "Fonction 33\n";
+    y();
+}
+
+function y() {
+    echo "Fonction 50\n";
+    z();
+}
+
+function z() {
+    echo "Fonction 10\n";
+    aa();
+}
+
+function aa() {
+    echo "Fonction 6\n";
+    bb();
+}
+
+function bb() {
+    echo "Fonction 12\n";
+    cc();
+}
+
+function cc() {
+    echo "Fonction 26\n";
+    dd();
+}
+
+function dd() {
+    echo "Fonction 18\n";
+    ee();
+}
+
+function ee() {
+    echo "Fonction 11\n";
+    ff();
+}
+
+function ff() {
+    echo "Fonction 45\n";
+    gg();
+}
+
+function gg() {
+    echo "Fonction 22\n";
+    hh();
+}
+
+function hh() {
+    echo "Fonction 21\n";
+    ii();
+}
+
+function ii() {
+    echo "Fonction faut\n";
+    jj();
+}
+
+function jj() {
+    echo "Fonction 13\n";
+    kk();
+}
+
+function kk() {
+    echo "Fonction 20\n";
+    ll();
+}
+
+function ll() {
+    echo "Fonction 17\n";
+    mm();
+}
+
+function mm() {
+    echo "Fonction 49\n";
+    nn();
+}
+
+function nn() {
+    echo "sauver 47\n";
+    oo();
+}
+
+function oo() {
+    echo "Fonction 27\n";
+    pp();
+}
+
+function pp() {
+    echo "Fonction 30\n";
+    qq();
+}
+
+function qq() {
+    echo "Fonction 37\n";
+    rr();
+}
+
+function rr() {
+    echo "Fonction 48\n";
+    ss();
+}
+
+function ss() {
+    echo "Fonction 41\n";
+    tt();
+}
+
+function tt() {
+    echo "Fonction 31\n";
+    uu();
+}
+
+function uu() {
+    echo "Fonction 43\n";
+    vv();
+}
+
+function vv() {
+    echo "Fonction 29\n";
+    ww();
+}
+
+function ww() {
+    echo "Fonction 39\n";
+    xx();
+}
+
+function xx() {
+    echo "Fonction 25\n";
+    yy();
+}
+
+function yy() {
+    echo "Fonction 44\n";
+    zz();
+}
+
+function zz() {
+    echo "Fonction 16\n";
+    aaa();
+}
+
+function aaa() {
+    echo "Fonction 38\n";
+    bbb();
+}
+
+function bbb() {
+    echo "Fonction 36\n";
+    ccc();
+}
+
+function ccc() {
+    echo "Fonction 32\n";
+    ddd();
+}
+
+function ddd() {
+    echo "Fonction 40\n";
+    eee();
+}
+
+function eee() {
+    echo "Fonction 57\n";
+    fff();
+}
+
+function fff() {
+    echo "Fonction 56\n";
+    ggg();
+}
+
+function ggg() {
+    echo "Fonction 30\n";
+    hhh();
+}
+
+function hhh() {
+    echo "Fonction 37\n";
+    iii();
+}
+
+function iii() {
+    echo "Fonction 36\n";
+    jjj();
+}
+
+function jjj() {
+    echo "Fonction 38\n";
+    kkk();
+}
+
+function kkk() {
+    echo "Fonction la 39\n";
+    lll();
+}
+
+function lll() {
+    echo "Fonction 40\n";
+    mmm();
+}
+
+function mmm() {
+    echo "Fonction 41\n";
+    nnn();
+}
+
+function nnn() {
+    echo "Fonction 42\n";
+    ooo();
+}
+
+function ooo() {
+    echo "Fonction 43\n";
+    ppp();
+}
+
+function ppp() {
+    echo "Fonction 44\n";
+    qqq();
+}
+
+function qqq() {
+    echo "Fonction 45\n";
+    rrr();
+}
+
+function rrr() {
+    echo "Fonction 46\n";
+    sss();
+}
+
+function sss() {
+    echo "Fonction 47\n";
+    ttt();
+}
+
+function ttt() {
+    echo "Fonction 48\n";
+    uuu();
+}
+
+function uuu() {
+    echo "Fonction 49\n";
+    vvv();
+}
+
+function vvv() {
+    echo "Fonction 50\n";
+    www();
+}
+
+function www() {
+    echo "Fonction 51\n";
+    xxx();
+}
+
+function xxx() {
+    echo "Fonction 52\n";
+    yyy();
+}
+
+function yyy() {
+    echo "Fonction 53\n";
+    zzz();
+}
+
+function zzz() {
+    echo "Fonction 54\n";
+    aaaa();
+}
+
+function aaaa() {
+    echo "Fonction 55\n";
+    bbbb();
+}
+
+function bbbb() {
+    echo "Fonction 56\n";
+    cccc();
+}
+
+function cccc() {
+    echo "Fonction planete 57\n";
+    dddd();
+}
+
+function dddd() {
+    echo "Fonction 58\n";
+    fonction_homerde();
+}
+
+function fonction_homerde() {
+    echo '
+    <div class="homer">
+        <p>
+            WUT ? <br>
+            ▓▓▓▓<br>
+            ▒▒▒▓▓<br>
+            ▒▒▒▒▒▓<br>
+            ▒▒▒▒▒▒▓<br>
+            ▒▒▒▒▒▒▓<br>
+            ▒▒▒▒▒▒▒▓<br>
+            ▒▒▒▒▒▒▒▓▓▓<br>
+            ▒▓▓▓▓▓▓░░░▓<br>
+            ▒▓░░░░▓░░░░▓<br>
+            ▓░░░░░░▓░▓░▓<br>
+            ▓░░░░░░▓░░░▓<br>
+            ▓░░▓░░░▓▓▓▓<br>
+            ▒▓░░░░▓▒▒▒▒▓<br>
+            ▒▒▓▓▓▓▒▒▒▒▒▓<br>
+            ▒▒▒▒▒▒▒▒▓▓▓▓<br>
+            ▒▒▒▒▒▓▓▓▒▒▒▒▓<br>
+            ▒▒▒▒▓▒▒▒▒▒▒▒▒▓<br>
+            ▒▒▒▓▒▒▒▒▒▒▒▒▒▓<br>
+            ▒▒▓▒▒▒▒▒▒▒▒▒▒▒▓<br>
+            ▒▓▒▓▒▒▒▒▒▒▒▒▒▓<br>
+            ▒▓▒▓▓▓▓▓▓▓▓▓▓<br>
+            ▒▓▒▒▒▒▒▒▒▓<br>
+            ▒▒▓▒▒▒▒▒▓<br>
+        </p>
+    </div>';
+}
+a();
+?>
